100136998d304bec9e274eb0747d9f03ef34b950
Ronak Jain <ronak.jain@dianapps.com>
2025-07-23 11:07:15 +0530

    Created a client to check the generic graph

--- src/graphs/genericGraph.ts
+++ src/graphs/genericGraph.ts
@@ -3,20 +3,33 @@ import { NodeDown } from '../nodes/nodeDown';
 import { NodeLow } from '../nodes/nodeLow';
 import { NodeUp } from '../nodes/nodeUp';
 import { NodeHigh } from '../nodes/nodeHigh';
-import { StateGraph, START } from '@langchain/langgraph';
+import { StateGraph, START, END } from '@langchain/langgraph';
+
+var counter = 0;
 
 
 const edgeRouting = (state: GraphState) => {
-    switch (state.workflow.nodes[state.counter].langgraphNodeToUse) {
-        case 'NodeDown':
-            return 'nodeDown';
-        case 'NodeUp':
-            return 'nodeUp';
-        case 'NodeHigh':
-            return 'nodeHigh';
-        case 'NodeLow':
-            return 'nodeLow';
+    // 0 -> NodeHigh
+    console.log('state.counter :', counter);
+    console.log('state.workflow :', state.workflow);
+    console.log('state.workflow.nodes[state.counter] :', state.workflow.nodes[counter]);
+    if(!state.workflow.nodes[counter]) {
+        counter = 0;
+        return END;
     }
+    // switch (state.workflow.nodes[state.counter].langgraphNodeToUse) {
+    //     case 'NodeDown':
+    //         return 'nodeDown';
+    //     case 'NodeUp':
+    //         return 'nodeUp';
+    //     case 'NodeHigh':
+    //         return 'nodeHigh';
+    //     case 'NodeLow':
+    //         return 'nodeLow';
+    // }
+    counter += 1;
+    console.log('state.counter :', counter);
+    return 'nodeHigh';
     // ATTENTION_RONAK: the counter must be incremented somewhere. I'm not sure if GraphState can be mutated by an edge, but each node could do it when returning from its invoke method.
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { runRemoteGraph as runGrafumilo } from './testClients/clientGrafumilo.js';
+import { runRemoteGraph as runGenericGraph } from './testClients/clientGenericGraph.js';
 
 if (process.env.NODE_ENV === 'grafumilo') {
     runGrafumilo();
+}
+
+if (process.env.NODE_ENV === 'genericGraph') {
+    runGenericGraph();
 }
