5f36e1144dd1be2a1e558bdefffb07664975acef
René Stavnes <renestavnes@hotmail.com>
2025-07-30 07:23:35 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import { runRemoteGraph as runWorkflowRunner } from './testClients/clientWorkflowRunner.js';
 import { validateWorkflow } from './utils.js';
-import { calculatorJobs, calculatorWorkflowSpec_1 } from './mocks/calculator.js';
+import { calculatorJobs, calculatorWorkflowSpec_1, calculatorWorkflowSpec_2 } from './mocks/calculator.js';
+import { adapterAutodockJobs, adapterAutodockWorkflowSpec_1 } from './mocks/adapter_autodock.js';
 
-if (true || process.env.NODE_ENV === 'workflowRunner') {
+
+if (process.env.NODE_ENV === 'workflowRunner') {
     runWorkflowRunner();
 }
 
-if (false && process.env.NODE_ENV === 'workflowValidator') { // ATTENTION: hack
+if (process.env.NODE_ENV === 'workflowValidator') {
     try {
         console.log('Starting workflow validation...');
-        /* const result1 = validateWorkflow(Array.from(numericalJobs.values()), numericalWorkflow_1);
+        const result1 = validateWorkflow(Array.from(calculatorJobs.values()), calculatorWorkflowSpec_1.workflow);
         console.log('Validation result 1:', result1);
         console.log('------------------------------------------------------------');
-        const result2 = validateWorkflow(Array.from(numericalJobs.values()), numericalWorkflow_2);
+        const result2 = validateWorkflow(Array.from(calculatorJobs.values()), calculatorWorkflowSpec_2.workflow);
         console.log('Validation result 2:', result2);
-        console.log('------------------------------------------------------------'); */
-        /* const result3 = validateWorkflow(Array.from(calculatorJobs.values()), calculatorWorkflow_1);
-        console.log('Validation result 3:', result3); */
+        console.log('------------------------------------------------------------');
+        const result3 = validateWorkflow(Array.from(adapterAutodockJobs.values()), adapterAutodockWorkflowSpec_1.workflow);
+        console.log('Validation result 3:', result3);
     } catch (error) {
         console.error('Error during validation:', error);
         throw error;
